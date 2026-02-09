5be4f88e075b72ff787de622b49b84391f2d2c03
anisa <anisa.dsouza@gmail.com>
2026-02-09 12:13:54 +0530

    calling functions

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 #calculator
+import addition as a 
+import substraction as s
+import multiplication as m 
+
+print("Addition result = ", a.add(5,4))
+print("Substraction result = ", s.sub(9,5))
+print("Multiplication result = ", m.mul(5,5))
