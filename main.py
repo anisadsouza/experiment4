e42896f4887e869fb611e696badade40cf08f761
anisa <anisa.dsouza@gmail.com>
2026-02-09 12:39:55 +0530

    division function added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #calculator
 import addition as a 
 import substraction as s
-import multiplication as m 
+import multiplication as m
+import division as d 
 #calling funcations
 print("Addition result = ", a.add(5,4))
 print("Substraction result = ", s.sub(9,5))
 print("Multiplication result = ", m.mul(5,5))
+print("Division result = ", d.div(4,4))
 #this is calc
 #here called division functions
